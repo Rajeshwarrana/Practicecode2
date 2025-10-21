409db63c5136508939ea34c985b9e13840ef0241
Rajeshwarrana <rajeshwarrana09@gmail.com>
2025-10-21 20:19:37 +0530

    PYHTON AND C / C++ CODE

---
+++ .c & .cpp/54446.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int money;
+    
+    cout<<"ENTER MONEY YOU HAVE :- ";
+    cin>>money;
+    
+
+    cout<<"YOU HAVE";
+    cout<<money;
+
+    return 0;
+    
+
+ 
+        
+    
+}
